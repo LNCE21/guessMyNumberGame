baf03807b871b498cdcd7c679ff08f0fc384f364
LNCE21 <lautaronicolascc@gmail.com>
2021-10-20 00:14:32 -0300

    Selected an element on html through js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 'use strict';
+//Method available in the document object to select css element and his text content with .textContent property
+console.log(document.querySelector(".message").textContent);
