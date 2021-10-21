45a1875502c700ae82dba876ad0737a639ebb48e
LNCE21 <lautaronicolascc@gmail.com>
2021-10-21 19:21:32 -0300

    Added styling to the -game over- and -winning- scenarios

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 //Variable with random number between 1 and 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+
 //This variable can also be called "state variable" because is part of the application state (all the data relevant for the application)
 let score = 10;
 
@@ -19,10 +19,15 @@ document.querySelector(".check").addEventListener("click",
             document.querySelector(".message").textContent = "Enter a number!";
             //When the user wins  
         } else if (guess === secretNumber) {
+            document.querySelector(".number").textContent = secretNumber;
             document.querySelector(".message").textContent = "Correct number!";
+            document.querySelector("body").style.backgroundImage = "-webkit-radial-gradient(center, #3fa8c6 0%,#339cb9 25%, #0f3d6e 100%)";
+            document.querySelector(".number").style.width = "20rem";
+            document.querySelector(".number").style.fontSize = "8rem";
             //When the user enters an input out of the allowed range
         } else if (guess > 20 || guess < 1) {
             document.querySelector(".message").textContent = "Enter a number from 1 to 20";
+
             //When the user inputs a valid number
         } else if (guess > secretNumber) {
             //When the input is to high
@@ -30,9 +35,12 @@ document.querySelector(".check").addEventListener("click",
                 document.querySelector(".message").textContent = "Too high";
                 score--;
                 document.querySelector(".score").textContent = score;
-                //When the user looses the game
             } else {
+                //When the user looses the game
                 document.querySelector(".message").textContent = "GAME OVER!";
+                document.querySelector("body").style.backgroundImage = "-webkit-linear-gradient(top, #ffc719 0%, #bf033b 100%)";
+                document.querySelector(".number").textContent = secretNumber;
+                document.querySelector(".score").textContent = "0";
             }
         } else if (guess < secretNumber) {
             //When the input is to low
@@ -40,9 +48,12 @@ document.querySelector(".check").addEventListener("click",
                 document.querySelector(".message").textContent = "Too low";
                 score--;
                 document.querySelector(".score").textContent = score;
-                //When the user looses the game
             } else {
+                //When the user looses the game
                 document.querySelector(".message").textContent = "GAME OVER!";
+                document.querySelector("body").style.backgroundImage = "-webkit-linear-gradient(top, #ffc719 0%, #bf033b 100%)";
+                document.querySelector(".number").textContent = secretNumber;
+                document.querySelector(".score").textContent = "0";
             }
         }
     }
